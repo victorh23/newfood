43e119cbf337eee306038b7c804f5b92199d6197
Victor Uchoa <63361671+victorh23@users.noreply.github.com>
2022-10-07 10:28:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+// Abre e fecha o menu
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+// Fecha o menu ao clicar nele
+const links = document.querySelectorAll("nav ul li a");
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+// Scroll Reveal 
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+// Setando os IDs e classes da página
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #plans header, #plans .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  {
+    interval: 100,
+  }
+);
+
+// Função - Alterar header
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // adiciona se o scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // remove se o scroll for menor que a altura do header
+    header.classList.remove("scroll");
+  }
+}
+
+// Função - Voltar para o topo
+const backToTopButton = document.querySelector(".back-to-top");
+function backtoTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+// Chamada das funções pelo scroll
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backtoTop();
+  activateMenuAtCurrentSection();
+});
+
+// Menu ativo conforme a sessão visível
+const sections = document.querySelectorAll("main section[id]");
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
